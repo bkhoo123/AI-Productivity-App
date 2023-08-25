ae7f6620d4d3a33639bdaa358b121031fab5368b
Brian Khoo <102838003+bkhoo123@users.noreply.github.com>
2023-08-25 13:44:20 -0700

    Cleaned Up Chatbot

--- my-app/src/components/Rubbish/index.jsx
+++ my-app/src/components/Rubbish/index.jsx
@@ -1,71 +1,118 @@
 import { useState } from "react";
 import axios from "axios";
+import { PacmanLoader } from 'react-spinners';
 
-const RubbishChatBot = () => {
-  const [messages, setMessages] = useState([]);
-  const [input, setInput] = useState("");
-  const [who, setWho] = useState("user");
+const RubbishChatBot = ({chatModal, setChatModal}) => {
+    const [messages, setMessages] = useState([]);
+    const [input, setInput] = useState("");
+    const [loading, setLoading] = useState(false);
+    const [color, setColor] = useState("#7FFFD4");
 
-  const sendMessage = async (e) => {
-    e.preventDefault();
+    if (loading) {
+      return (
+          <div className="flex flex-col justify-center items-center w-[100%] mt-[300px]">
+              <div className="flex flex-col items-center justify-center w-full h-full">
+                  <PacmanLoader color={color} size={100} />
+                  <div className="text-2xl mt-6 font-semibold text-center">
+                      Parsing through Reports
+                  </div>
+              </div>
+          </div>
+      );
+  }
+  
 
-    if (!input.trim()) return; // prevents empty messages
-
-    const payload = {
-      input: input,
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+        sendMessage();
     };
 
-    const response = await axios.post(
-      `/api/ChatGPT/RubbishChatBot/chatbot`,
-      payload
-    );
-    if (response) {
-      setMessages((prevMessages) => [
-        ...prevMessages,
-        { sender: 'User', text: input },
-        { sender: 'RubbishGPT', text: response.data.message.content },
-      ]);
-      setInput('');
-    }
-  };
+    const sendMessage = async () => {
+        if (!input.trim()) return; // prevents empty messages
 
-  return (
-    <div className="p-8 w-full">
-      <div className="mb-4 overflow-y-auto h-[50%] border-2 border-sky-400 rounded-md">
-      {messages.map((message, index) => (
-          <div key={index} className={`p-2 ${message.sender === 'RubbishGPT' ? 'text-blue-500 font-bold text-md' : 'text-green-500 font-bold text-md'}`}>
-            {message.sender}: {message.text}
-          </div>
-        ))}
-      </div>
-      <div className="flex">
-        <input
-          type="text"
-          value={input}
-          onChange={(e) => setInput(e.target.value)}
-          className="flex-grow p-2 border rounded"
-        />
-      </div>
-      <div className="p-6">
-        <button
-          onClick={sendMessage}
-          className="mx-2 p-3 bg-blue-500 text-white rounded font-semibold"
-        >
-          Send Message
-        </button>
-        <button
-          className="mx-4 p-3 bg-blue-500 text-white rounded font-semibold"
-        >
-          Clear Chat
-        </button>
-      </div>
-    </div>
-  );
+        const payload = {
+            input: input,
+        };
+        setLoading(true);
+
+        const response = await axios.post(
+            `/api/ChatGPT/RubbishChatBot/chatbot`,
+            payload
+        );
+        
+
+        if (response) {
+            setMessages((prevMessages) => [
+                ...prevMessages,
+                { sender: 'User', text: input },
+                { sender: 'RubbishGPT', text: response.data.message.content },
+            ]);
+            setInput('');
+            setLoading(false);
+        }
+    };
+
+    const clearMessage = () => {
+      setMessages([]);
+    };
+
+    return (
+        <div className="p-8 w-full ">
+            <div className="mb-4 overflow-y-auto flex flex-col gap-3 ">
+              <div className="flex justify-between">
+                <div className="text-2xl font-bold">
+                  Chat with RubbishGPT
+                </div>
+                <button 
+                  className="p-2 bg-blue-500 text-white font-semibold rounded-md"
+                  onClick={() => setChatModal(!chatModal)}
+                  >
+                    Close
+                </button>
+              </div>
+
+              <div className="border-t "/>
+              <div className="font-semibold mt-2 p-2 bg-gray-100 rounded-md max-w-[60%]">
+                Hi I am Rubbish GPT Chatbot. I can help you with your queries about Rubbish incidents and reports. Feel free to ask me any questions and I will do my utmost to give you the best answer I can.
+              </div>
+              {messages.map((message, index) => (
+                <div 
+                    key={index} 
+                    className={`flex ${message.sender === 'RubbishGPT' ? 'justify-start' : 'justify-end'} `}
+                >
+                    <div className={`${message.sender === 'RubbishGPT' ? 'text-black-500 font-semibold text-md bg-gray-100 p-2 rounded-md' : 'text-white bg-indigo-500 rounded-md p-2 font-semibold text-md'} max-w-[60%]`}>
+                        {message.text}
+                    </div>
+                </div>
+              ))}
+            </div>
+            
+            <form onSubmit={handleFormSubmit} className="flex flex-col gap-3">
+                <input
+                    type="text"
+                    value={input}
+                    onChange={(e) => setInput(e.target.value)}
+                    placeholder="Type a message..."
+                    className="flex-grow p-3 border rounded"
+                />
+                <div className="flex justify-between">
+                    <button
+                        type="submit"
+                        className="mx-2 p-3 bg-blue-500 text-white rounded font-semibold"
+                    >
+                        Send Message
+                    </button>
+                    <button
+                        type="button"
+                        className="mx-4 p-3 bg-blue-500 text-white rounded font-semibold"
+                        onClick={clearMessage}
+                    >
+                        Clear Chat
+                    </button>
+                </div>
+            </form>
+        </div>
+    );
 };
 
 export default RubbishChatBot;
-
-// Send Chat GPT the users question get the intent from it
-// Give it a example object
-// Middleware function to automatically filter through unnecessxary data and ask chatGPT to do this
-// data set too large
--- my-app/src/pages/dashboard.jsx
+++ my-app/src/pages/dashboard.jsx
@@ -1,26 +1,47 @@
-import React, {useState} from 'react'
-import SidePanel from '@/components/SidePanel'
-import NavBar from '@/components/Navbar'
-import JobSearch from '@/components/JobSearch'
-import Ebay from '@/components/eBay/Ebay'
-import Rubbish from '@/components/Rubbish'
+import React, { useState } from "react";
+import SidePanel from "@/components/SidePanel";
+import NavBar from "@/components/Navbar";
+import JobSearch from "@/components/JobSearch";
+import Ebay from "@/components/eBay/Ebay";
+import Rubbish from "@/components/Rubbish";
+import Chat from "./chat.png";
+import Image from "next/image";
+import Chatbot from "@/components/Rubbish";
 
 const Dashboard = () => {
-    const [activePanel, setActivePanel] = useState('JobSearch')
+  const [activePanel, setActivePanel] = useState("JobSearch");
+  const [chatModal, setChatModal] = useState(false);
+  
 
-    const panels = {
-      "JobSearch": <JobSearch />,
-      "Ebay": <Ebay />,
-      "Rubbish": <Rubbish />
-    }
+  const panels = {
+    JobSearch: <JobSearch />,
+    Ebay: <Ebay />,
+    Rubbish: <Rubbish />,
+  };
 
-    return (
-      <div className="flex flex-row">
-          <SidePanel activePanel={activePanel} setActivePanel={setActivePanel} />
-          {/* <NavBar /> */}
-          {panels[activePanel]}
+  return (
+    <div className="flex flex-row relative">
+      <SidePanel activePanel={activePanel} setActivePanel={setActivePanel} />
+      {/* <NavBar /> */}
+      {panels[activePanel]}
+
+      <button
+        className="absolute right-16 bottom-16 z-20"
+        onClick={() => setChatModal(!chatModal)}
+      >
+        <Image src={Chat} height={60} width={60} />
+      </button>
+
+      {chatModal && (
+        // Overlay for Dimmed Background
+        <div className="fixed top-0 left-0 w-full h-full bg-black opacity-50 z-10"></div>
+      )}
+
+      <div className={`${chatModal ? "z-30 absolute left-[10%] top-[10%] bg-white border-4  w-[80%] h-[75%] overflow-y-auto rounded-md" : 'hidden'}`}>
+        <Chatbot chatModal={chatModal} setChatModal={setChatModal}/>
       </div>
-    )
-}
+    </div>
+  );
+};
 
-export default Dashboard
+export default Dashboard;
